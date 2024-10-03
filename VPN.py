b89a1615af1214dfe7c10fe1525e0e2639794af5
Albert Williams <albertbpwilliams@gmail.com>
2024-10-03 13:44:52 -0400

    Uploaded project

---
+++ VPN.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import socket
+import arguments
+import argparse
+
+# Run 'python3 VPN.py --help' to see what these lines do
+parser = argparse.ArgumentParser('Send a message to a server at the given address and prints the response')
+parser.add_argument('--VPN_IP', help='IP address at which to host the VPN', **arguments.ip_addr_arg)
+parser.add_argument('--VPN_port', help='Port number at which to host the VPN', **arguments.vpn_port_arg)
+args = parser.parse_args()
+
+VPN_IP = args.VPN_IP  # Address to listen on
+VPN_PORT = args.VPN_port  # Port to listen on (non-privileged ports are > 1023)
+
+def parse_message(message):
+    message = message.decode("utf-8")
+    # Parse the application-layer header into the destination SERVER_IP, destination SERVER_PORT,
+    # and message to forward to that destination
+    raise NotImplementedError("Your job is to fill this function in. Remove this line when you're done.")
+    return SERVER_IP, SERVER_PORT, message
+
+### INSTRUCTIONS ###
+# The VPN, like the server, must listen for connections from the client on IP address
+# VPN_IP and port VPN_port. Then, once a connection is established and a message recieved,
+# the VPN must parse the message to obtain the server IP address and port, and, without
+# disconnecting from the client, establish a connection with the server the same way the
+# client does, send the message from the client to the server, and wait for a reply.
+# Upon receiving a reply from the server, it must forward the reply along its connection
+# to the client. Then the VPN is free to close both connections and exit.
+
+# The VPN server must additionally print appropriate trace messages and send back to the
+# client appropriate error messages.
---
+++ echo-server.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import socket
+import arguments
+import argparse
+
+# Run 'python3 echo-server.py --help' to see what these lines do
+parser = argparse.ArgumentParser('Starts a server that returns the data sent to it unmodified')
+parser.add_argument('--server_IP', help='IP address at which to host the server', **arguments.ip_addr_arg)
+parser.add_argument('--server_port', help='Port number at which to host the server', **arguments.server_port_arg)
+args = parser.parse_args()
+
+SERVER_IP = args.server_IP  # Address to listen on
+SERVER_PORT = args.server_port  # Port to listen on (non-privileged ports are > 1023)
+
+print("server starting - listening for connections at IP", SERVER_IP, "and port", SERVER_PORT)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((SERVER_IP, SERVER_PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected established with {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(f"Received client message: '{data!r}' [{len(data)} bytes]")
+            print(f"echoing '{data!r}' back to client")
+            conn.sendall(data)
+
+print("server is done!")
